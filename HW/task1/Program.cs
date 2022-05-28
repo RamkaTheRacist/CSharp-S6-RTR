3c1702d3f511006c25d0a4ee4fda7a3fa83b57ed
Ramka <whereisourgod@gmail.com>
2022-05-28 09:25:22 +0300

    task1

---
+++ HW/task1/Program.cs
@@ -0,0 +1,34 @@
+﻿//Показать двумерный массив размером m×n заполненный вещественными числами
+void FillArray(double[,] any, int minValue, int maxValue)
+{
+    for(int i = 0; i < any.GetLength(0); i++)  // 0 = 3
+{
+    for(int j = 0; j < any.GetLength(1); j++) // 1 = 4 and etc
+    {
+        any[i, j] = new Random().NextDouble() * (maxValue - minValue) + minValue;
+    }
+}
+}
+void PrintArray(double[,] any)
+{
+for(int i = 0; i < any.GetLength(0); i++)  // 0 = 3
+{
+    for(int j = 0; j < any.GetLength(1); j++) // 1 = 4 and etc
+    {
+        Console.Write($"{any[i, j]} ");
+    }
+Console.WriteLine();
+}
+}
+//Показать двумерный массив размером m×n заполненный целыми числами
+Console.WriteLine("How much rows in array?");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("How much columns in array?");
+int columns = int.Parse(Console.ReadLine());
+Console.WriteLine("Random number from?");
+int min = int.Parse(Console.ReadLine());
+Console.WriteLine("Random number to?");
+int max = int.Parse(Console.ReadLine());
+double[,] array = new double[rows, columns];
+FillArray(array, min, max);
+PrintArray(array);
